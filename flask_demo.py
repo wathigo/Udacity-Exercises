827d8d41bd473d6bd25e520546693bcd0b31224d
wathigo <wathigosimon@gmail.com>
2022-05-13 20:23:19 +0300

    Add a debugging class method, 'repr

--- flask_demo.py
+++ flask_demo.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/darkmode'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -13,8 +14,12 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
+    def __repr__(self):
+        return f'<Person ID: {self.id}, name: {self.name}>'
+
 db.create_all()    
 
 @app.route('/')
 def index():
-    return 'hello world'
+    person = Person.query.first()
+    return 'hello ' + person.name
