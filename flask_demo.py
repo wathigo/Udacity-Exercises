d42deff24bdade544f104ba338939182e12189a9
wathigo <wathigosimon@gmail.com>
2022-05-13 20:00:52 +0300

    Connect flask app to the database using sqlalchemy and create table using models

--- flask_demo.py
+++ flask_demo.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/darkmode'
+
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+db.create_all()    
 
 @app.route('/')
 def index():
