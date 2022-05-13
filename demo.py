b4aa8071949d070dd0fc13160734444f13836cfe
wathigo <wathigosimon@gmail.com>
2022-05-13 18:49:11 +0300

    Fetching data from the database to use in python

--- demo.py
+++ demo.py
@@ -21,6 +21,23 @@ data = {
     'completed': False
 }
 cursor.execute(query, data)
+cursor.execute('INSERT INTO table1 (id, completed) VALUES (%s, %s);', (3, True))
+
+cursor.execute('SELECT * FROM table1')
+
+results1 = cursor.fetchmany(2)
+print('fetchmany(2)', results1)
+
+results2 = cursor.fetchone()
+print('fetchone()', results2)
+
+results3 = cursor.fetchone()
+print('fetchone()', results3)
+
+cursor.execute('SELECT * FROM table1')
+
+results4 = cursor.fetchone()
+print('fetchone()', results4)
 
 connection.commit()
 cursor.close()
